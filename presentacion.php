0a4e2ad51dfeb8d0b353b91019caffe4c5287c4e
nachopdl <124316313+nachopdl@users.noreply.github.com>
2023-10-08 14:05:34 -0600

    Add files via upload

---
+++ presentacion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/presentacion_style.css">
+    <title>PaySafe</title>
+</head>
+<body>
+    <h1>Bienvenido a Tu Sitio Web</h1>
+    <div class="button-container">
+        <a href="login.php" class="button">Iniciar Sesión</a>
+        <a href="registro.php" class="button">Registrarse</a>
+    </div>
+</body>
+</html>
